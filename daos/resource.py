f9da4e86718f641037eeff8ebb0583b8a123913c
Julian <julian.quinones@upr.edu>
2020-03-13 10:21:52 -0400

    Added Dummy Resource Dao

--- daos/resource.py
+++ daos/resource.py
@@ -1,11 +1,11 @@
 import psycopg2
 class ResourceDAO:
-   def __init__(self):
+    def __init__(self):
 
-       #connection_url = "dbname=%s user=%s password=%s" % (pg_config['dbname'],
-        #                                                    pg_config['user'],
-         #                                                   pg_config['passwd'])
-        #self.conn = psycopg2._connect(connection_url)
+       connection_url = "dbname=%s user=%s password=%s" % ('dbname',
+                                                    'user',
+                                                          'passwd')
+       self.conn = psycopg2._connect(connection_url)
 
     def getAllResources(self):
         #cursor = self.conn.cursor()
