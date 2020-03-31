e8f1cd1af2c544cd0529e510b6ce83744a884d57
Zhaotian Wu Melendez <zhaotian.wu@upr.edu>
2020-03-30 23:05:51 -0400

    Request handler and DAOs

---
+++ daos/Water.py
@@ -0,0 +1,147 @@
+#import psycopg2
+class MedicalDeviceDAO:
+    def __init__(self):
+
+       connection_url = "dbname=%s user=%s password=%s" % ('dbname',
+                                                    'user',
+                                                          'passwd')
+       #self.conn = psycopg2._connect(connection_url)
+
+    def getAllWater(self):
+        #cursor = self.conn.cursor()
+        #query = "select pid, pname, pmaterial, pcolor, pprice from parts;"
+        #cursor.execute(query)
+        row={};
+        result = [];
+        row[0] = 'Test'
+        row[1] = 'Get'
+        row[2] = 'All'
+        row[3] = 'Water'
+        row[4] = 'Test'
+
+
+        result.append(row)
+        #for row in cursor:
+        #    result.append(row)
+        return result
+
+    def getWaterById(self, wid):
+       # cursor = self.conn.cursor()
+        #query = "select pid, pname, pmaterial, pcolor, pprice from parts where pid = %s;"
+        #cursor.execute(query, (pid,))
+        #result = cursor.fetchone()
+       row = {};
+       row[0] = '12345'
+       row[1] = 'Get'
+       row[2] = 'Water'
+       row[3] = 'by'
+       row[4] = 'ID'
+
+
+       return row
+
+    def getWaterBySize(self, wsize):
+        #cursor = self.conn.cursor()
+        #query = "select * from parts where pmaterial = %s;"
+        #cursor.execute(query, (material,))
+        #result = []
+        #for row in cursor:
+        #    result.append(row)
+        row = {};
+        result = [];
+        row[0] = '1 Gallon'
+        row[1] = 'Get'
+        row[2] = 'Water'
+        row[3] = 'by'
+        row[4] = 'Size'
+
+        result.append(row)
+        return result
+
+    def getWaterByBrand(self, wbrand):
+        # cursor = self.conn.cursor()
+        # query = "select * from parts where pcolor = %s;"
+        # cursor.execute(query, (color,))
+        # result = []
+        # for row in cursor:
+        #    result.append(row)
+        row = {};
+        result = [];
+        row[0] = 'FIJI'
+        row[1] = 'Get'
+        row[2] = 'Water'
+        row[3] = 'By'
+        row[4] = 'Brand'
+
+        result.append(row)
+        return result
+
+    def insert(self, rid, wbrand, wsize, wdescription):
+        #cursor = self.conn.cursor()
+        #query = "insert into parts(pname, pcolor, pmaterial, pprice) values (%s, %s, %s, %s) returning pid;"
+        #cursor.execute(query, (pname, pcolor, pmaterial, pprice,))
+        #pid = cursor.fetchone()[0]
+        #self.conn.commit()
+        row = {};
+        result = [];
+        row[0] = 'Inserting:'
+        row[1] = 'RID'
+        row[2] = 'BRAND'
+        row[3] = 'SIZE'
+        row[4] = 'DESCRIPTION'
+
+        result.append(row)
+        return rid
+
+    def delete(self, wid):
+        #cursor = self.conn.cursor()
+        #query = "delete from parts where pid = %s;"
+        #cursor.execute(query, (pid,))
+        #self.conn.commit()
+        row = {};
+        result = [];
+        row[0] = 'DELETING:'
+        row[1] = 'WATER'
+        row[2] = 'ID: 12345'
+
+        result.append(row)
+        return wid
+
+    def update(self, wbrand, wsize, wdescription):
+        #cursor = self.conn.cursor()
+        #query = "update parts set pname = %s, pcolor = %s, pmaterial = %s, pprice = %s where pid = %s;"
+        #cursor.execute(query, (pname, pcolor, pmaterial, pprice, pid,))
+        #self.conn.commit()
+        row = {};
+        result = [];
+        row[0] = 'UPDATING:'
+        row[1] = 'updatename'
+        row[2] = 'updatetype'
+        row[4] = 'updatelocation'
+        row[5] = 'updatesid'
+
+        result.append(row)
+        return wbrand
+
+    def getResourceID(self,wid):
+        rid = wid+2;  #dummy code
+        #select rid from Water where wid = %s;
+        return rid
+
+    def getCountByWaterId(self):
+        #cursor = self.conn.cursor()
+        #query = "select pid, pname, sum(stock) from parts natural inner join supplies group by pid, pname order by pname;"
+        #cursor.execute(query)
+        #result = []
+        #for row in cursor:
+        #    result.append(row)
+        row = {};
+        result = [];
+        row[0] = 'A'
+        row[1] = 'B'
+        row[2] = 'C'
+        row[3] = 'D'
+        row[4] = 't98765'
+
+        result.append(row)
+        return row
---
+++ handler/Water_handler.py
@@ -0,0 +1,194 @@
+from flask import jsonify
+
+from daos.Water import WaterDAO
+
+from daos.resource import ResourceDAO
+
+
+class WaterHandler:
+    def build_water_dict(self, row):
+        result = {};
+        result['wid'] = row[0]
+        result['rid'] = row[1]
+        result['brand'] = row[2]
+        result['size'] = row[3]
+        result['wdescription'] = row[4]
+        return result
+
+    def build_supplier_dict(self, row):
+        result = {}
+        result['sid'] = row[0]
+        result['stype'] = row[1]
+        result['sname'] = row[2]
+        result['semail'] = row[3]
+        result['sphone'] = row[4]
+        result['saddress'] = row[5]
+        result['sfinance'] = row[6]
+        return result
+
+    def build_requester_dict(self, row):
+        result = {}
+        result['uid'] = row[0]
+        result['ReqID'] = row[1]
+        result['rfirstname'] = row[2]
+        result['rlastname'] = row[3]
+        result['remail'] = row[4]
+        result['rphone'] = row[5]
+        result['raddress'] = row[6]
+        result['rlocation'] = row[6]
+        return result
+
+    def build_Water_attributes(self, wid, rid, brand, size, wdescription):
+        result = {};
+        result['wid'] = wid
+        result['rid'] = rid
+        result['brand'] = brand
+        result['size'] = size
+        result['wdescription'] = wdescription
+        return result
+
+    def getAllWater(self):
+        dao = WaterDAO()
+        Water_list = dao.getAllWater()
+        result_list = []
+        for row in Water_list:
+            result = self.build_Water_dict(row)
+            result_list.append(result)
+        return jsonify(Results=result_list)
+
+    def getWaterById(self, wid):
+        dao = WaterDAO()
+        row = dao.getWaterById(wid)
+        if not row:
+            return jsonify(Error = "Water Not Found"), 404
+        else:
+            Water = self.build_Water_dict(row)
+            return jsonify(Water = Water)
+
+    def getWaterBySize(self, size):
+        dao = WaterDAO()
+        Waterlist = dao.getWaterBySize(size)
+        result_list = []
+        if not Waterlist:
+            return jsonify(Error = "No Water Found with the specified Size"), 404
+        else:
+            for row in Waterlist:
+                result = self.build_Water_dict(row)
+                result_list.append(result)
+            return jsonify(Water = result_list)
+
+    def getWaterByBrand(self, brand):    #Filter by Water category
+        dao = WaterDAO()
+        Waterlist = dao.getWaterByBrand(brand)
+        result_list = []
+        if not Waterlist:
+            return jsonify(Error = "No Water Found with the Specified Brand"), 404
+        else:
+            for row in Waterlist:
+                result = self.build_Water_dict(row)
+                result_list.append(result)
+            return jsonify(Water = result_list)
+
+    def insertWater(self, form):
+        print("form: ", form)
+        if len(form) != 7:
+            return jsonify(Error = "Malformed post request"), 400
+        else:
+            rname = form['rname']
+            rtype = form['rtype']
+            rlocation = form['rlocation']
+            sid = form['sid']
+
+            brand = form['brand']
+            size = form['rname']
+            wdescription = form['wdescription']
+
+            if rtype and rname and rlocation and sid and brand and size and wdescription:
+                resourcedao = ResourceDAO()
+                dao = WaterDAO()
+                rid = resourcedao.insert(rtype,rname,rlocation,sid)
+                wid = dao.insert(rid, brand, size, wdescription)
+                result = self.build_Water_attributes(wid, rid, brand, size, wdescription)
+                return jsonify(Water=result), 201
+            else:
+                return jsonify(Error="Unexpected attributes in post request"), 400
+
+    def insertWaterJson(self, json):
+
+        rname = json['rname']
+        rtype = json['rtype']
+        rlocation = json['rlocation']
+        sid = json['sid']
+        brand = json['brand']
+        size = json['size']
+        wdescription = json['wdescription']
+
+
+        if rtype and rname and rlocation and sid and brand and size and wdescription:
+            resourcedao = ResourceDAO()
+            dao = WaterDAO()
+            rid = resourcedao.insert(rtype, rname, rlocation, sid)
+            wid = dao.insert(rid, brand, size, wdescription)
+            result = self.build_Water_attributes(wid, rid, brand, size, wdescription)
+            return jsonify(Water=result), 201
+
+        else:
+            return jsonify(Error="Unexpected attributes in post request"), 400
+
+    def deleteWater(self, wid):
+        dao = WaterDAO()
+        resourceDAO = ResourceDAO()
+
+        if not dao.getWaterById(wid):
+            return jsonify(Error = "Water not found."), 404
+        else:
+            rid = dao.getResourceID(wid)
+            dao.delete(wid)
+            resourceDAO.delete(rid)
+            return jsonify(DeleteStatus = "OK"), 200
+
+    def updateWater(self, wid, form):
+        resourceDAO = ResourceDAO()
+        dao = WaterDAO()
+        if not dao.getWaterById(wid):
+            return jsonify(Error = "Water not found."), 404
+        else:
+            if len(form) != 7:
+                return jsonify(Error="Malformed update request"), 400
+            else:
+                rname = form['rname']
+                rtype = form['rtype']
+                rlocation = form['rlocation']
+                sid = form['sid']
+
+                brand = form['brand']
+                size = form['size']
+                wdescription = form['wdescription']
+
+                rid = dao.getResourceID(wid)
+
+                if rtype and rname and rlocation and sid and brand and size and wdescription:
+                    dao.update(brand, size, wdescription)
+                    resourceDAO.update(rname,rtype,rlocation)
+                    result = self.build_Water_attributes(wid, rid, brand, size,wdescription)
+                    return jsonify(Water=result), 200
+                else:
+                    return jsonify(Error="Unexpected attributes in update request"), 400
+
+    def build_Water_counts(self, Water_counts):
+        result = []
+        #print(part_counts)
+        for P in Water_counts:
+            D = {}
+            D['id'] = P[0]
+            D['name'] = P[1]
+            D['count'] = P[2]
+            result.append(D)
+        return result
+
+    def getCountByWaterId(self):
+        dao = WaterDAO()
+        result = dao.getCountByWaterId()
+        #print(self.build_part_counts(result))
+        #return jsonify(WaterCounts = self.build_Water_counts(result)), 200
+        return jsonify(WaterCounts=result), 200
