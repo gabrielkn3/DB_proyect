b198da4a7327d7129224313fdbb34ff7e505584a
gabrielkn3 <Xemar250>
2020-05-05 02:02:52 -0400

    --Made some changes in main.py:   -Edited some urls for companies   -now companies filters by name and type through request.forms. --supplier.py and requester.py all gets work

--- daos/supplier.py
+++ daos/supplier.py
@@ -3,10 +3,6 @@ import psycopg2
 from daos.user import userDAO
 
 class SupplierDAO:
-    global supplier_list, s_id, removed
-    supplier_list = []
-    s_id = 0
-    removed=0
     def __init__(self):
 
         connection_url = "dbname=%s user=%s host = '*' password=%s" % (database_config['dbname'],
@@ -14,13 +10,6 @@ class SupplierDAO:
                                                             database_config['passwd'])
         self.conn = psycopg2._connect(connection_url)
 
-
-    def first_time(self):
-        row = {}
-        row[0] = 1150
-        row[1] = 12321
-        row[2] = "Lares"
-        supplier_list.append(row)
 
     def getAllSuppliers(self):
         cursor = self.conn.cursor()
@@ -34,7 +23,7 @@ class SupplierDAO:
 
     def getSupplierById(self, sid):
         cursor = self.conn.cursor()
-        query = "select sid, uid, slocation from supplier where sid = %s;"
+        query = "select * from supplier where sid = %s;"
         cursor.execute(query, (sid,))
         result = cursor.fetchone()
         return result
@@ -55,10 +44,12 @@ class SupplierDAO:
         return result
 
     def getSuppliersByLocation(self, slocation):
-        query = "select sid, uid, slocation from supplier where slocation = %s;"
+        query = "select * from supplier where slocation = %s;"
         cursor = self.conn.cursor()
         cursor.execute(query, (slocation,))
-        result = cursor.fetchone()
+        result = []
+        for row in cursor:
+            result.append(row)
         return result
 
 
@@ -72,28 +63,14 @@ class SupplierDAO:
 
 ################################################################################################################################################################################
     def insert(self, uid, slocation):
-        row={}
-        row[0] = len(supplier_list)
-        row[1] = uid
-        row[2] = slocation
-        supplier_list.append(row)
-        return s_id
+
+        return None
 
     def delete(self, sid):
-        for i in range(0, len(supplier_list)):
-            for row in supplier_list:
-                if row[0] == sid:
-                    supplier_list.remove(row)
-                    removed =1
-                    return sid
+
         return None #failed
 
     def update(self, sid, slocation):
-        result = []
-        for i in range(0, len(supplier_list)):
-            for row in supplier_list:
-                if row[0] == sid:
-                    row[2] = slocation
-                    result.append(row)
 
-        return result
+
+        return None
